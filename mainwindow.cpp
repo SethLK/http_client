6cc7302e50ef873e37f4ccfab7654ae08dadc508
SethLK <sethlk2006@gmail.com>
2024-03-17 15:50:23 +0630

    First commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -64,6 +64,48 @@ void MainWindow::handleAction()
             manager->deleteLater();
         });
     }
+    else if(method == "POST"){
+        QString jsonDataString = ui->textEdit->toPlainText();
+
+        QJsonParseError parseError;
+        QJsonDocument jsonDocument = QJsonDocument::fromJson(jsonDataString.toUtf8(), &parseError);
+
+        if (parseError.error != QJsonParseError::NoError) {
+            qDebug() << "Error parsing JSON:" << parseError.errorString();
+            return;
+        }
+
+        QNetworkAccessManager* manager = new QNetworkAccessManager(this);
+        QNetworkRequest request(QUrl(ui->lineEdit->text()));
+        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+        QNetworkReply* reply = manager->post(request, jsonDocument.toJson());
+        connect(reply, &QNetworkReply::finished, [=]() {
+            if (reply->error() != QNetworkReply::NoError) {
+                qDebug() << "Error:" << reply->errorString();
+            } else {
+                // Read the response data
+                QByteArray responseData = reply->readAll();
+
+                // Parse JSON data
+                QJsonDocument jsonResponse = QJsonDocument::fromJson(responseData);
+
+                // Check if JSON parsing was successful
+                if (jsonResponse.isNull()) {
+                    qDebug() << "Error: Failed to parse JSON response";
+                } else {
+                    // Print the JSON data
+                    qDebug().noquote() << "Response:" << jsonResponse.toJson(QJsonDocument::Indented);
+                    ui->textBrowser->append("Json :\n" + QString(jsonResponse.toJson(QJsonDocument::Indented)));
+                }
+            }
+
+            // Clean up
+            reply->deleteLater();
+            manager->deleteLater();
+        });
+    }
+
 
     // For demonstration purposes, let's display the method and input text in the text edit
     ui->textBrowser->append("Method: " + method);
